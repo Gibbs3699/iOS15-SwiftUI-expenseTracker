62badf8260318fb257a3d7dbe89b32cf42485c86
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-05-24 14:08:00 +0700

    Initial Commit

---
+++ ExpenseTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpenseTracker
+//
+//  Created by TheGIZzz on 24/5/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by TheGIZzz on 24/5/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
